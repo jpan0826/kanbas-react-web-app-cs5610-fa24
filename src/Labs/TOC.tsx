31785a559d4cacba75b6e832cbc2246413e677fa
Ju Pan <panju0826@gmail.com>
2024-09-18 23:06:45 -0700

    added github repo

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,6 +7,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><a target="_blank" href="https://github.com/jpan0826/kanbas-react-web-app-cs5610-fa24">Github</a></li>
 
       </ul>
     );
