d8b0874ff5ae1b7eaa7ed960b337853f6f5e1362
Ju Pan <panju0826@gmail.com>
2024-10-30 14:45:29 -0700

    updated hidden buttons

--- src/Kanbas/Courses/Assignments/AssignmentControlButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControlButtons.tsx
@@ -3,16 +3,18 @@ import GreenCheckmark from "./GreenCheckmark";
 import { FaTrash } from "react-icons/fa";
 import { useSelector } from "react-redux";
 export default function AssignmentControlButton(
-  { assignmentId, deleteAssignment } : {assignmentId: string; deleteAssignment: (assignmentId: string) => void; }
+  { assignmentId, deleteAssignment }: { assignmentId: string; deleteAssignment: (assignmentId: string) => void; }
 ) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div className="float-end">
 
-      <GreenCheckmark />
-      <IoEllipsisVertical className="fs-4" />
-      {currentUser.role==='FACULTY' &&
-      <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteAssignment(assignmentId)}/>
-      }
+      {currentUser.role === 'FACULTY' && (<div>
+        <GreenCheckmark />
+        <IoEllipsisVertical className="fs-4" />
+
+        <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteAssignment(assignmentId)} />
+      </div>)}
     </div>
-);}
+  );
+}
--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -12,10 +12,12 @@ export default function ModuleControlButtons(
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div className="float-end">
-      {currentUser.role==='FACULTY' && (<FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />)}
-      {currentUser.role==='FACULTY' && (<FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)}/>)}
+      {currentUser.role==='FACULTY' && (<div>
+      <FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />
+      <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)}/>
       <GreenCheckmark />
       <BsPlus />
       <IoEllipsisVertical className="fs-4" />
+      </div>)}
     </div>
 );}
