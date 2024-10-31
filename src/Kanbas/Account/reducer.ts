9a7376574e7d1d2db32b24b7a1049a170f2cf43f
Ju Pan <panju0826@gmail.com>
2024-10-30 23:10:47 -0700

    implemented enrollments

--- src/Kanbas/Account/reducer.ts
+++ src/Kanbas/Account/reducer.ts
@@ -1,6 +1,9 @@
+import * as db from "../Database";
 import { createSlice } from "@reduxjs/toolkit";
+const emptyStringArray : String[] = [];
 const initialState = {
   currentUser: null,
+  enrollments: emptyStringArray
 };
 const accountSlice = createSlice({
   name: "account",
@@ -8,7 +11,14 @@ const accountSlice = createSlice({
   reducers: {
     setCurrentUser: (state, action) => {
       state.currentUser = action.payload;
+      state.enrollments = db.enrollments.filter((enrollment)=>enrollment.user === action.payload._id).map((enrollment)=> enrollment.course)
     },
+    enroll: (state, action) => {
+      state.enrollments = [...state.enrollments, action.payload]
+    },
+    unenroll: (state, action) => {
+      state.enrollments = state.enrollments.filter((enrollment)=>enrollment != action.payload)
+    }
   },
 });
 export const { setCurrentUser } = accountSlice.actions;
