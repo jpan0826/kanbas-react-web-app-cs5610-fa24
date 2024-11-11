94cf1cecf05a4f6e2a1706b744f0808670d8f220
Ju Pan <panju0826@gmail.com>
2024-11-11 00:39:47 -0800

    3.5.6

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -4,6 +4,8 @@ import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
 import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
     return (
@@ -21,6 +23,8 @@ export default function Lab5() {
         <WorkingWithObjects />
         <WorkingWithArrays />
         <HttpClient />
+        <WorkingWithObjectsAsynchronously />
+        <WorkingWithArraysAsynchronously />
       </div>
     );
     
