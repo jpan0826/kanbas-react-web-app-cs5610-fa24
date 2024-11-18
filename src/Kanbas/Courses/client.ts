3441d5f0f85ee60a3ef595b3c7b92dc70b626cfa
Ju Pan <panju0826@gmail.com>
2024-11-18 15:52:26 -0800

    completed 4.5.4

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+
+export const createModuleForCourse = async (courseId: string, module: any) => {
+    const response = await axios.post(
+        `${COURSES_API}/${courseId}/modules`,
+        module
+    );
+    return response.data;
+};
+
 export const fetchAllCourses = async () => {
-  const { data } = await axios.get(COURSES_API);
-  return data;
+    const { data } = await axios.get(COURSES_API);
+    return data;
 };
 
 
@@ -15,5 +24,11 @@ export const deleteCourse = async (id: string) => {
 export const updateCourse = async (course: any) => {
     const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
     return data;
-  };
-  
+};
+
+export const findModulesForCourse = async (courseId: string) => {
+    const response = await axios
+        .get(`${COURSES_API}/${courseId}/modules`);
+    return response.data;
+};
+
