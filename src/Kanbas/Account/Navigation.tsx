d64e446ee760b44a1575d97652aab454f31e86c8
Ju Pan <panju0826@gmail.com>
2024-11-24 22:39:07 -0800

    completed 3.7.2

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 export default function AccountNavigation() {
+  const { pathname } = useLocation();
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link id="wd-account-signin-link" to="/Kanbas/Account/Signin"  className="list-group-item active border border-0" > Signin  </Link> <br/>
-      <Link id="wd-account-signup-link" to="/Kanbas/Account/Signup" className="list-group-item text-danger border border-0"  > Signup  </Link> <br/>
-      <Link id="wd-account-profile-link" to="/Kanbas/Account/Profile" className="list-group-item text-danger border border-0" > Profile </Link> <br/>
+      {links.map((link) => (
+        <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item ${active(link)}`}> {link} </Link>
+      ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link>)}
+
     </div>
-);}
+  );
+}
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -49,3 +49,21 @@ export const unenroll = async (courseId: string) => {
     return response.data;
 }
 
+export const findAllUsers = async () => {
+    const response = await axiosWithCredentials.get(USERS_API);
+    return response.data;
+};
+
+export const findUsersByRole = async (role: string) => {
+    const response = await
+        axios.get(`${USERS_API}?role=${role}`);
+    return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+    const response = await axios.get(`${USERS_API}?name=${name}`);
+    return response.data;
+};
+
+
+
